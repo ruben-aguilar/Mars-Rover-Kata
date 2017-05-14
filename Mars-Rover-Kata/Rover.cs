25cba1fb0c52550dd199db3837de1f914de42756
Rubén Aguilar Becerra <ruben.aguilar.becerra@outlook.es>
2017-05-14 11:56:24 +0200

    GivenARoverInX0Y0AndInNorthDirection_WhenMoveForward_ItGoesToX0Y1

--- Mars-Rover-Kata/Rover.cs
+++ Mars-Rover-Kata/Rover.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mars_Rover_Kata
 {
-    class Rover
+    public class Rover
     {
+        private int m_X = 0;
+        private int m_Y = 0;
+        private char m_direction = 'N';
+
+        public int X { get { return m_X; } }
+        public int Y { get { return m_Y; } }
+        public char Direction { get { return m_direction; } }
+
+        public Rover(int x, int y, char direction)
+        {
+            m_X = x;
+            m_Y = y;
+            m_direction = direction;
+        }
+
+        public void ExecuteCommand(char command)
+        {
+            m_Y = 1;
+        }
+
+
     }
 }
--- RoverTests/RoverTests.cs
+++ RoverTests/RoverTests.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using Mars_Rover_Kata;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RoverTests
 {
+
+
+
+
     [TestClass]
     public class RoverTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Rover m_rover;
+
+        [TestInitialize]
+        public void SetUp()
         {
+            m_rover = new Rover(0, 0, 'N');
         }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            m_rover = null;
+        }
+
+        [TestMethod]
+        public void WhenCreateAMarsRoverWithCoordinatesAndDirection_ThenTheRoverHasTheseCoordinatesAndDirection()
+        {
+            int X = 1;
+            int Y = 1;
+            char direction = 'N';
+
+            Rover rover = new Rover(X, Y, direction);
+
+            Assert.AreEqual(X, rover.X);
+            Assert.AreEqual(Y, rover.Y);
+            Assert.AreEqual(direction, rover.Direction);
+        }
+
+        [TestMethod]
+        public void GivenARoverInX0Y0AndInNorthDirection_WhenMoveForward_ItGoesToX0Y1()
+        {
+            m_rover.ExecuteCommand('F');
+
+            Assert.AreEqual(m_rover.X, 0);
+            Assert.AreEqual(m_rover.Y, 1);
+        }
+
     }
 }
